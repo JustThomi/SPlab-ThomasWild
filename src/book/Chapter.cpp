67017a00c630a0dbaf16dec9f13ed0ae532dec90
JustThomi <t.wild155@gmail.com>
2023-10-18 12:54:26 +0300

    fix print and add .gitignore

--- src/book/Chapter.cpp
+++ src/book/Chapter.cpp
@@ -9,6 +9,10 @@ Chapter::~Chapter() {}
 // Print method to display the chapter name
 void Chapter::print() const {
     std::cout << "Chapter Name: " << name << std::endl;
+
+    for (SubChapter* subChapter: subchapters){
+        subChapter->print();
+    }
 }
 
 void Chapter::addSubChapter(std::string subchapterName){
